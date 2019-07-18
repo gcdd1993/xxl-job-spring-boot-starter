bdeef38c69b4b3b88ea441083512d98554faaac5
gaochen <gaochen@maxtropy.com>
2019-07-18 17:32:42 +0800

    添加示例代码

--- src/test/java/io/github/gcdd1993/xxljob/autoconfigure/XxlJobSpringBootStarterApplicationTests.java
+++ src/test/java/io/github/gcdd1993/xxljob/autoconfigure/XxlJobSpringBootStarterApplicationTests.java
@@ -3,6 +3,7 @@ package io.github.gcdd1993.xxljob.autoconfigure;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,6 +15,10 @@ public class XxlJobSpringBootStarterApplicationTests {
 
     @Autowired
     private XxlJobAutoConfiguration xxlJobAutoConfiguration;
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobSpringBootStarterApplicationTests.class, args);
+    }
 
     @Test
     public void contextLoads() {
